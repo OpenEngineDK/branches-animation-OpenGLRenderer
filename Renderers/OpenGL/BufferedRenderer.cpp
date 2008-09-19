7351c6031421e15030a002cda433e4792a8fef99
cpvc <cpvc@openengine.dk>
2008-09-19 11:07:41 +0000

    added BufferedRenderer

---
+++ Renderers/OpenGL/BufferedRenderer.cpp
@@ -0,0 +1,176 @@
+#include <Renderers/OpenGL/BufferedRenderer.h>
+
+#include <Logging/Logger.h>
+
+namespace OpenEngine {
+namespace Renderers {
+namespace OpenGL {
+
+BufferedRenderer::BufferedRenderer() {
+    fbo = depthbuffer = img = 0;
+    width = 800;
+    height = 600;
+}
+
+BufferedRenderer::~BufferedRenderer() {}
+
+void BufferedRenderer::Handle(InitializeEventArg arg) {
+    CHECK_FOR_GL_ERROR();
+
+    // make sure that there is a gl context
+    Renderer::Handle(arg);
+    CHECK_FOR_GL_ERROR();
+
+    //@todo: check that the FBO extension is supported
+
+    glGenFramebuffersEXT(1, &fbo);
+    CHECK_FOR_GL_ERROR();
+
+    glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fbo);
+    CHECK_FOR_GL_ERROR();
+
+    
+    // Adding a depth buffer
+    glGenRenderbuffersEXT(1, &depthbuffer);
+    CHECK_FOR_GL_ERROR();
+    glBindRenderbufferEXT(GL_RENDERBUFFER_EXT, depthbuffer);
+    CHECK_FOR_GL_ERROR();
+    glRenderbufferStorageEXT(GL_RENDERBUFFER_EXT,GL_DEPTH_COMPONENT, 
+                             width, height);
+    CHECK_FOR_GL_ERROR();
+    glFramebufferRenderbufferEXT(GL_FRAMEBUFFER_EXT,
+                                 GL_DEPTH_ATTACHMENT_EXT,
+                                 GL_RENDERBUFFER_EXT, depthbuffer);
+    CHECK_FOR_GL_ERROR();
+
+    // Adding a Texture
+    glGenTextures(1, &img);
+    CHECK_FOR_GL_ERROR();
+    glBindTexture(GL_TEXTURE_2D, img);
+    CHECK_FOR_GL_ERROR();
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8,
+                 width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+    CHECK_FOR_GL_ERROR();
+    glFramebufferTexture2DEXT(GL_FRAMEBUFFER_EXT, 
+                              GL_COLOR_ATTACHMENT0_EXT, 
+                              GL_TEXTURE_2D, img, 0);
+    CHECK_FOR_GL_ERROR();
+
+    // check FBO state for errors
+#if OE_DEBUG_GL
+    GLenum status = glCheckFramebufferStatusEXT(GL_FRAMEBUFFER_EXT);
+    if (status!=GL_FRAMEBUFFER_COMPLETE_EXT)
+        throw Exception("incomplete frame buffer object");
+#endif
+
+    // done initializing, go back to the main gl context 
+    glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0); //unbind
+    CHECK_FOR_GL_ERROR();
+}
+
+void BufferedRenderer::Handle(ProcessEventArg arg) {
+    CHECK_FOR_GL_ERROR();
+
+    glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fbo); //bind
+    CHECK_FOR_GL_ERROR();
+    glPushAttrib(GL_VIEWPORT_BIT);
+    CHECK_FOR_GL_ERROR();
+    glViewport(0,0,width, height);
+    CHECK_FOR_GL_ERROR();
+
+    //test: glDrawBuffer(GL_COLOR_ATTACHMENT0_EXT);
+
+    Renderer::Handle(arg);
+
+    CHECK_FOR_GL_ERROR();
+    glPopAttrib();
+    CHECK_FOR_GL_ERROR();
+    glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0); //unbind
+    CHECK_FOR_GL_ERROR();
+
+    // for debugging
+    RenderTextureInOrtho();
+    CHECK_FOR_GL_ERROR();
+
+    /*  
+    unsigned int i = 0-1;
+    while(i--);
+    logger.info << "frame done" << logger.end;
+    */
+}
+
+void BufferedRenderer::RenderTextureInOrtho() {
+
+    GLboolean t = glIsEnabled(GL_TEXTURE_2D);
+    GLboolean l = glIsEnabled(GL_LIGHTING);
+    GLboolean d = glIsEnabled(GL_DEPTH_TEST);
+
+    // render a screen sized quad
+    glDisable(GL_DEPTH_TEST);
+    CHECK_FOR_GL_ERROR();
+    glDisable(GL_LIGHTING);
+    CHECK_FOR_GL_ERROR();
+    glEnable(GL_TEXTURE_2D);
+    CHECK_FOR_GL_ERROR();
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+    CHECK_FOR_GL_ERROR();
+
+    // quad rendering of texture
+    glBindTexture(GL_TEXTURE_2D, img);
+    CHECK_FOR_GL_ERROR();
+
+    glMatrixMode(GL_PROJECTION);
+    glPushMatrix();
+    glLoadIdentity();
+    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+    CHECK_FOR_GL_ERROR();
+
+    glMatrixMode(GL_MODELVIEW);
+    glPushMatrix();
+    glLoadIdentity();
+    CHECK_FOR_GL_ERROR();
+
+    glViewport(0, 0, width, height);
+    CHECK_FOR_GL_ERROR();
+
+    glBegin(GL_QUADS);
+
+    glTexCoord2f(0.0, 0.0);
+    glVertex3f(-1.0, -1.0, 0.5);
+
+    glTexCoord2f(1.0, 0.0);
+    glVertex3f(1.0, -1.0, 0.5);
+
+    glTexCoord2f(1.0, 1.0);
+    glVertex3f(1.0, 1.0, 0.5);
+
+    glTexCoord2f(0.0, 1.0);
+    glVertex3f(-1.0, 1.0, 0.5);
+
+    glEnd();
+    CHECK_FOR_GL_ERROR();
+
+    glMatrixMode(GL_PROJECTION);
+    glPopMatrix();
+    glMatrixMode( GL_MODELVIEW );
+	glPopMatrix();
+    CHECK_FOR_GL_ERROR();
+
+    if (!t) glDisable(GL_TEXTURE_2D);
+    if (l) glEnable(GL_LIGHTING);
+    if (d) glEnable(GL_DEPTH_TEST);
+    CHECK_FOR_GL_ERROR();
+}
+
+void BufferedRenderer::Handle(DeinitializeEventArg arg) {
+    glDeleteFramebuffersEXT(1, &fbo);
+    CHECK_FOR_GL_ERROR();
+    glDeleteRenderbuffersEXT(1, &depthbuffer);
+    CHECK_FOR_GL_ERROR();
+    Renderer::Handle(arg);
+    CHECK_FOR_GL_ERROR();
+}
+
+} // NS OpenGL
+} // NS OpenEngine
+} // NS Renderers
---
+++ Renderers/OpenGL/BufferedRenderer.h
@@ -0,0 +1,48 @@
+// OpenGL buffered renderer implementation.
+// renderers a scene to a 2d texture using a Frame Buffer Object (FBO)
+//
+// initial code based on:
+// http://www.gamedev.net/reference/articles/article2331.asp
+// -------------------------------------------------------------------
+// Copyright (C) 2007 OpenEngine.dk (See AUTHORS) 
+// 
+// This program is free software; It is covered by the GNU General 
+// Public License version 2 or any later version. 
+// See the GNU General Public License for more details (see LICENSE). 
+//--------------------------------------------------------------------
+
+#ifndef _BUFFERED_RENDERER_H_
+#define _BUFFERED_RENDERER_H_
+
+#include <Meta/OpenGL.h>
+#include <Renderers/OpenGL/Renderer.h>
+
+namespace OpenEngine {
+namespace Renderers {
+namespace OpenGL {
+
+/**
+ * BufferedRenderer using OpenGL
+ *
+ * @class BufferedRenderer BufferedRenderer.h Renderers/OpenGL/Renderer.h
+ */
+class BufferedRenderer : public Renderer {
+public:
+    BufferedRenderer();
+    ~BufferedRenderer();
+
+    void Handle(InitializeEventArg arg);
+    void Handle(ProcessEventArg arg);
+    void Handle(DeinitializeEventArg arg);
+private:
+    GLuint fbo, depthbuffer, img;
+    unsigned int width, height;
+
+    void RenderTextureInOrtho();
+};
+
+} // NS OpenGL
+} // NS OpenEngine
+} // NS Renderers
+
+#endif // _BUFFERED_RENDERER_H_
